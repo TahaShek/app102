1db46a592555d458cf64e14e66785a210e1b6d8b
TahaSekk <sohailtaha128@gmail.com>
2022-08-25 00:29:20 +0500

    commit

--- src/app/admin-module/admin-module-routing.module.ts
+++ src/app/admin-module/admin-module-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboradComponent } from './admin-Component/dashboard/dashborad/dashborad.component';
+import { ProductsComponent } from './admin-Component/products/products/products.component';
 import { AdminModuleComponent } from './admin-module.component';
 
-const routes: Routes = [{ path: '', component: AdminModuleComponent }];
+const routes: Routes = [{ path: '', component: AdminModuleComponent ,
+ children:[
+{path:'dash-board',component:DashboradComponent},
+{path:'products',component:ProductsComponent},
+ ]
+
+
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin-module/admin-module.module.ts
+++ src/app/admin-module/admin-module.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminModuleRoutingModule } from './admin-module-routing.module';
 import { AdminModuleComponent } from './admin-module.component';
+import { DashboradComponent } from './admin-Component/dashboard/dashborad/dashborad.component';
+import { ProductsComponent } from './admin-Component/products/products/products.component';
 
 
 @NgModule({
   declarations: [
-    AdminModuleComponent
+    AdminModuleComponent,
+    DashboradComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,

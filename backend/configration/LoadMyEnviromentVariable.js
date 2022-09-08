3a4c542aadab74cfebee16c90d4c6f7f4ceff314
TahaShek <sohailtaha128@gmail.com>
2022-09-09 01:43:14 +0500

    coomit

--- backend/configration/LoadMyEnviromentVariable.js
+++ backend/configration/LoadMyEnviromentVariable.js
@@ -3,13 +3,13 @@ const dotenv=require('dotenv')
 let MyEnviroment={};
 
 if(process.env.NODE_ENV === 'testing'){
-    MyEnviroment=dotenv.config({path:`${__dirname}/../application-testing-enviroment`})
+    MyEnviroment=dotenv.config({path:`${__dirname}/../application-testing-enviroment.env`})
 }
 if(process.env.NODE_ENV === 'development'){
-    MyEnviroment=dotenv.config({path:`${__dirname}/../application-development-enviroment`})
+    MyEnviroment=dotenv.config({path:`${__dirname}/../application-development-enviroment.env`})
 }
 if(process.env.NODE_ENV === 'production'){
-    MyEnviroment=dotenv.config({path:`${__dirname}/../application-production-enviroment`})
+    MyEnviroment=dotenv.config({path:`${__dirname}/../application-production-enviroment.env`})
 }
 
 console.log(MyEnviroment)

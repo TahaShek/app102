33767b59fbe429a98bb1f52f340fb58b069d819a
TahaShek <sohailtaha128@gmail.com>
2022-09-10 12:31:54 +0500

    commt

---
+++ backend/configration/LoadMyEnviromentVaraibles.js
@@ -0,0 +1,6 @@
+const dotenv=require('dotenv')
+let MyEnviroment={}
+if(process.env.NODE_ENV==='testing'){
+    MyEnviroment=dotenv.config({path:`${__dirname}/../application-testing-enviroment.env`})
+}
+console.log(MyEnviroment)
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,7 @@
 const express=require('express')
 const app=express()
+const loadVariables=require('./configration/LoadMyEnviromentVaraibles')
+const PORT=process.env.PORT
+app.listen(PORT,()=>{
+    console.log(`your port is ${PORT}`)
+})
